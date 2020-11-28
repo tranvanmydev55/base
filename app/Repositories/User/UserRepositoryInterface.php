7fdabbfa38d58db20aed9fdf580c3768edfd3374
tranvanmy <anhmynd19101995@gmail.com>
2020-11-28 22:49:00 +0700

    feat(Backend): Remove folder

--- app/Repositories/User/UserRepositoryInterface.php
+++ app/Repositories/User/UserRepositoryInterface.php
@@ -11,32 +11,9 @@ use App\Repositories\BaseRepositoryInterface;
  */
 interface UserRepositoryInterface extends BaseRepositoryInterface
 {
-    public function getUserByRole($role);
-
-    public function getOrderByToPost();
-
     public function getProfile($user);
-
-    public function getByName($keyword);
-
-    public function getBySearchLike($keywords);
-
-    public function getByIds($ids, $removeBlock = true);
-
-    public function getBusinessAccountByIds($ids);
-
-    public function suggestion($lat, $long, $validIds, $inValidIds, $phones);
 
     public function getProfileAdmin($userId);
 
-    public function getAccountForSearch();
-
-    /**
-     * Get Profile Detail account for CMS
-     *
-     * @param request
-     *
-     * @return model
-     */
     public function getProfileAccountCms($id);
 }
